0bdfa41957a98345dd03de6359e0896b1db20bf8
anubhavmishra800 <127845673+anubhavmishra800@users.noreply.github.com>
2023-05-22 15:05:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+let data=document.getElementsByTagName("strong");
+	for(let i=0;i<data.length;i++){
+		data[i].className="strong_color"
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+let data=document.getElementsByTagName("strong");
+	for(let i=0;i<data.length;i++){
+		data[i].className="strong_normal"
+	}
     
 }
